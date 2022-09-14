82709a14db5bebe198a54c2eef65e37ebb85f9bb
Ibrahim Yashar <ibo7777@abv.bg>
2022-09-14 16:53:57 +0300

    support for statements

--- src/com/craftinginterpreters/Stmt.java
+++ src/com/craftinginterpreters/Stmt.java
@@ -3,34 +3,37 @@ package com.craftinginterpreters;
 import java.util.List;
 
 abstract class Stmt {
-  interface Visitor<R> {
-    R visitExpressionStmt(Expression stmt);
-    R visitPrintStmt(Print stmt);
-  }
-  static class Expression extends Stmt {
-    Expression(Expr expression) {
-      this.expression = expression;
+    interface Visitor<R> {
+        R visitExpressionStmt(Expression stmt);
+
+        R visitPrintStmt(Print stmt);
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitExpressionStmt(this);
+    static class Expression extends Stmt {
+        Expression(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
+
+        final Expr expression;
     }
 
-    final Expr expression;
-  }
-  static class Print extends Stmt {
-    Print(Expr expression) {
-      this.expression = expression;
+    static class Print extends Stmt {
+        Print(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitPrintStmt(this);
+        }
+
+        final Expr expression;
     }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitPrintStmt(this);
-    }
-
-    final Expr expression;
-  }
-
-  abstract <R> R accept(Visitor<R> visitor);
+    abstract <R> R accept(Visitor<R> visitor);
 }
